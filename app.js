a52adff0b204e5dfaed982bb1e93503448796e53
ccarcache <ceciliocarcache@gmail.com>
2021-04-13 23:12:08 +0000

    Finalizando ejercicio de pruebas con jest

---
+++ app.js
@@ -0,0 +1,38 @@
+// declaramos una funcion con el mismo nombre "formEuroToDollar"
+// one euro is:
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+function fromDollarToYen(dollar){
+    var yenes = (dollar / oneEuroIs["USD"]) * oneEuroIs["JPY"];
+        return yenes;
+}
+
+function fromEuroToDollar(euro){
+    var dollar1 = euro * oneEuroIs["USD"];
+        return dollar1;
+}
+
+function fromYenToPound(yen){
+    let euro = (yen / oneEuroIs.JPY); //yen a euros
+     let conver = euro * oneEuroIs.GBP;
+        return conver;
+}
+
+console.log(fromDollarToYen(150));
+console.log(fromEuroToDollar(150));
+console.log(fromYenToPound(1279));
+// this is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+// just a console log for ourselves.
+console.log(sum(7,3))
+
+// export the function to be used on other files 
+// (similar to the keyword `export` when using webpack)
+module.exports = { sum, fromEuroToDollar }
