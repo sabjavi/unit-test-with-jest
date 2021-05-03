c6995b82e5d6244387118cd2a37146fde9eaf2ac
sabjavi <jasabal@hotmail.com>
2021-05-03 19:57:06 +0000

    unit test con jest

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // declaramos una funcion con el mismo nombre "formEuroToDollar"
 // one euro is:
 let oneEuroIs = {
-    "JPY": 127.9, // japan yen
-    "USD": 1.2, // us dollar
-    "GBP": 0.8, // british pound
+    "JPY": 128.3, // japan yen
+    "USD": 1.35, // us dollar
+    "GBP": 0.9, // british pound
 }
 
 function fromDollarToYen(dollar){
